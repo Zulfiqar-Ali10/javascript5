c550498b7ad26ffaedfe7ad2b62d8dadcf7df265
Zulfiqar-Ali10 <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2024-03-30 16:35:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,154 @@
+
+///////////////////////////////////////////Start project no 5////////////////////////////////////
+
+// Question 1
+// var multiArray = [[]];
+
+// Question 2
+// var Matrix = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+
+
+
+// Question 3
+// for (var i = 1; i <=10; i++) {
+//     document.write(i + '<br>');
+// }
+
+
+// Question 4
+// var UserNumber = +prompt("Enter Your Number");
+// var UserLength = +prompt("Enter Your Length");
+// for (let i = 1; i <= UserLength; i++) {
+//     console.log(UserNumber * i);
+// }
+
+
+
+
+// Question 5 
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (var i = 0; i <fruits.length; i++) {
+//     document.write(fruits[i]+'<br>');
+// }
+// for (var i = 0; i < fruits.length; i++) {
+//     document.write("Element at index " + i + " is " + fruits[i] + '<br>');
+// }
+// through nested loop
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//     document.write(fruits[i] + '<br>');
+
+//     for (var j = 0; j < fruits.length; j++) {
+//         if (i === j) {
+//             document.write("Element at index " + j + " is " + fruits[j] + '<br>');
+//         }
+//     }
+// }
+
+
+
+// Question 6
+// document.write("Counting:");
+// for (let i = 1; i <= 15; i++) {
+//     document.write(i +",");
+// }
+// document.write("\nReverse counting:");
+// for (let i = 10; i >= 1; i--) {
+// document.write(i + "," );
+//  }
+// document.write("\nEven:");
+// for (let i = 0; i <= 20; i += 2) {
+//     document.write(i  + ",");
+// }
+//  document.write("\nOdd:");
+//  for (let i = 1; i <= 19; i += 2) {
+//      document.write(i  + ",");
+//  }
+// document.write("\nSeries:");
+// for (let i = 1; i <= 10; i++) {
+//     document.write(2 * i + "k"  + ",");
+// }
+
+
+
+
+
+
+// Question 7
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var userInput = prompt("Enter the item you want to search:");
+// var found = false;
+
+// // Convert user input to lowercase for case-insensitive comparison
+// userInput = userInput.toLowerCase();
+
+// for (var i = 0; i < A.length; i++) {
+//     if (A[i] === userInput) {
+//         found = true;
+//         break;
+//     }
+// }
+
+// if (found) {
+//     document.write(userInput + " is available at index " + i + " in our bakery.");
+// } else {
+//     document.write("Sorry, " + userInput + " is not available in our bakery.");
+// }
+
+
+
+//  Question 8
+// var A = [24, 53, 78, 91, 12];
+// var largest = A[0];
+// for (let i = 0; i < A.length; i++) {
+//     if (largest < A[i]) {
+//         largest = A[i];
+//     }
+// }
+
+
+// Question 9
+// var a = [24, 53, 78, 91, 12];
+// var smallest = a[0]; // Assume the first number is the smallest
+
+// for (var i = 1; i < a.length; i++) {
+//     if (a[i] < smallest) {
+//         smallest = a[i];
+//     }
+// }
+
+
+// Question 10
+// for (let i = 5; i <= 100; i += 5) {
+//     console.log(i);
+// }
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+/////////////////////////////////////////////End project no 5//////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
